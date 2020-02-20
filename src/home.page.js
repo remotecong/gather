6d272c7ef4f0fb9c11e0d565e79c3ca0db00c91b
Dillon Christensen <j.dillon.christensen@gmail.com>
2020-02-19 23:29:34 -0600

    adapted homepage to use glamor

--- src/home.page.js
+++ src/home.page.js
@@ -1,15 +1,44 @@
 import React from 'react';
-import './home.page.css';
 import SearchComponent from "./search.component";
 import OwnerInfo from "./owner-info";
 
+import { css } from 'glamor';
+
+const styles = {
+    container: css({
+        fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
+        fontSize: 16,
+        padding: '1rem',
+        '@media (min-width: 1050px)': {
+            margin: 'auto',
+            width: 900,
+        },
+    }),
+    title: css({
+        borderBottom: 'solid 1rem springgreen',
+        display: 'inline-block',
+        fontSize: '4rem',
+        margin: 'auto auto .75rem',
+        '@media (min-width: 1050px)': {
+            margin: '0 0 2rem',
+        },
+    }),
+    about: css({
+        fontSize: '1.25rem',
+    }),
+    logo: css({
+        height: '3rem',
+        '@media (min-width: 1050px)': {
+            height: '7rem',
+        },
+    }),
+};
+
 export default () => (
-    <section className="homepage">
-        <h1 className="homepage__title"><img className="homepage__logo" src="logo.gif" alt="Gather Logo" /> Gather</h1>
-        <p className="homepage__about">Reverse address lookup for finding owners and their phone numbers</p>
-        <div className="homepage__split-panel">
-            <SearchComponent/>
-            <OwnerInfo />
-        </div>
-    </section>
+    <div {...styles.container}>
+        <h1 {...styles.title}><img {...styles.logo} src="logo.gif" alt="Gather Logo" /> Gather</h1>
+        <p {...styles.about}>Reverse address lookup for finding owners and their phone numbers</p>
+        <SearchComponent/>
+        <OwnerInfo />
+    </div>
 );
